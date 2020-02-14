8649be787ab39f5a76a08c6a41de81118d6cd21b
Szigeti Péter <45334856+plungingChode@users.noreply.github.com>
2020-02-14 18:11:30 +0100

    jo sorrendben szinez

--- vasarely.cpp
+++ vasarely.cpp
@@ -1,74 +1,93 @@
 #include "graphics.hpp"
 using namespace genv;
 
-const int WINDOW_X = 600;
-const int WINDOW_Y = 600;
+const int SIZE_X = 600;
+const int SIZE_Y = 600;
+
+void vasarely(int hezag, int margo)
+{
+    const int mid_x = SIZE_X / 2;
+    const int mid_y = SIZE_Y / 2;
+
+    const int max_x = SIZE_X - margo;
+    const int max_y = SIZE_Y - margo;
+
+    const int min_xy = margo;
+
+    const int cycles = (SIZE_X - margo) / (2 * hezag);
+
+    for (int i = 0; i < cycles; i++)
+    {
+        int color_diff = i * 255 / cycles;
+        gout << color(255 - color_diff, color_diff, 255);
+        gout << move_to(mid_x + i * hezag, mid_y)
+             << line_to(mid_x + i / hezag, min_xy + i * hezag);        
+
+        gout << move_to(mid_x - i * hezag, mid_y)
+             << line_to(mid_x - i / hezag, max_y - i * hezag);
+
+        gout << color(color_diff, 255 - color_diff, 255);
+        gout << move_to(mid_x + i * hezag, mid_y)
+             << line_to(mid_x + i / hezag, max_y - i * hezag);
+
+        gout << move_to(mid_x - i * hezag, mid_y)
+             << line_to(mid_x - i / hezag, min_xy + i * hezag);
+    }
+
+    gout << color(255, 0, 255)
+         << move_to(mid_x , min_xy)
+         << line_to(mid_x , max_y)
+         << move_to(min_xy, mid_y)
+         << line_to(max_x , mid_y);
+}
 
 void clear_screen()
 {
-    gout << color(0,0,0);
-    gout << move_to(0,0) << box_to(WINDOW_X - 1, WINDOW_Y - 1);
-    gout << refresh;
-}
-
-void vasarely(unsigned vonalkoz)
-{
-    gout<<move_to(300,300);
-    unsigned lepesek = 300 / vonalkoz;
-
-    for(unsigned i=0; i < lepesek; i++)
-    {
-        unsigned char diff = (i * 255 / lepesek);
-        gout << color(0, 255 - diff, diff);
-        
-        // jobb felso
-        gout << move_to(300 + i * vonalkoz, 300);
-        gout << line_to(300 + i / vonalkoz, 0 + i * vonalkoz);
-
-        // jobb also
-        gout << move_to(600 - i * vonalkoz, 300);
-        gout << line_to(300 + i / vonalkoz, 300 + i * vonalkoz);
-
-        // bal felso
-        gout << move_to(300 - i * vonalkoz, 300);
-        gout << line_to(300 - i / vonalkoz, 0 + i * vonalkoz);
-
-        // bal also
-        gout << move_to(0   + i * vonalkoz, 300);
-        gout << line_to(300 + i / vonalkoz, 300 + i * vonalkoz);
-    }
-
-    gout << color(0, 255, 0);
-    gout << move_to(300, 0) << line_to(300,600);
-    gout << move_to(0, 300) << line_to(600,300);
-    gout << refresh;
+    gout << color(0, 0, 0)
+         << move_to(0, 0)
+         << box_to(SIZE_X - 1, SIZE_Y - 1);
 }
 
 int main()
 {
-    gout.open(WINDOW_X, WINDOW_Y);
-    // unsigned vonalkoz = 10;
-    vasarely(10);
+    gout.open(SIZE_X, SIZE_Y);
+
+    int hezag = 9;
+    int margo = 25;
+    vasarely(hezag, margo);
+    gout << refresh;
 
     event ev;
-    while(gin >> ev)
-    {
-        // if (ev.type == ev_key)
-        // {
-        //     if (ev.keycode == key_up)
-        //     {
-        //         vonalkoz += 1;
-        //         clear_screen();
-        //         vasarely(vonalkoz);
-        //     }
-
-        //     if (ev.keycode == key_down && vonalkoz > 0)
-        //     {
-        //         vonalkoz -= 1;
-        //         clear_screen();
-        //         vasarely(vonalkoz);
-        //     }
-        // }
+    while(gin >> ev) 
+    {        
+        if (ev.type == ev_key)
+        {
+            if (ev.keycode == key_up)
+            {
+                hezag++;
+                clear_screen();
+                vasarely(hezag, margo);
+            }
+            else if (ev.keycode == key_down && hezag > 1)
+            {
+                hezag--;
+                clear_screen();
+                vasarely(hezag, margo);
+            }
+            else if (ev.keycode == 43) // numpad +
+            {
+                margo += 5;
+                clear_screen();
+                vasarely(hezag, margo);
+            }
+            else if (ev.keycode == 45 && margo >= 5) // numpad -
+            {
+                margo -= 5;
+                clear_screen();
+                vasarely(hezag, margo);
+            }
+            gout << refresh;
+        }
     }
     return 0;
 }
